aa6d9f851d2c70ee18c9107d8889b4d936d105d8
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2020-10-16 10:48:32 -0300

    Criação dos métodos

--- carro.py
+++ carro.py
@@ -4,3 +4,12 @@ class Carro:
         self.motor = motor
         self.direcao = direcao
         self.eletrico = eletrico
+
+    def partida(self):
+        print('Dando partida no veículo...')
+
+    def acelera(self):
+        print('Carro acelerado em movimento...')
+
+    def parar(self):
+        print('Carro parado!')
